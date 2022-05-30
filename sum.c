a8a1ac28e7fa52a909137efa410f68abcc9a49bc
Haripreetam2004 <101517540+Haripreetam2004@users.noreply.github.com>
2022-05-30 09:37:34 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+//this helps us to know the sum of to values
+#include <stdio.h>
+main ()
+{
+	int num1,num2,sum;
+	printf("Assign the value for num1:");
+	scanf ("%d",&num1);
+	printf("Assign the value for num2:");
+	scanf ("%d",&num2);
+	sum=num1+num2;
+	printf ("the sum  of num1 and num2 is %d",sum);
+}
