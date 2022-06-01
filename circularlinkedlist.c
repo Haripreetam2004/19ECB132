0229d384b01683aaefd66072b373baa7618639dd
Haripreetam2004 <101517540+Haripreetam2004@users.noreply.github.com>
2022-06-01 09:59:02 +0530

    Update circularlinkedlist.c

--- circularlinkedlist.c
+++ circularlinkedlist.c
@@ -88,7 +88,7 @@ void main() {
    printf("\nList after deleting all items: ");
    printList();   
 }
-output:
+/*output:
 Original List: 
 [ (6,56) (5,40) (4,1) (3,30) (2,20)  ]
 Deleted value:(6,56) 
@@ -98,4 +98,4 @@ Deleted value:(3,30)
 Deleted value:(2,20) 
 Deleted value:(1,10) 
 List after deleting all items: 
-[  ]
+[  ]*/
